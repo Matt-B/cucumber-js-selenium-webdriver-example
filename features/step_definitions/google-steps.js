f8c2316cac2aec78c0579571428543bd8e065ba2
Matt Bailey <matt.bailey@axonvibe.com>
2016-05-20 12:57:32 +0100

    Modify step definitions to return promises instead of using callbacks.

--- features/step_definitions/google-steps.js
+++ features/step_definitions/google-steps.js
@@ -5,23 +5,19 @@ var expect = require('chai').expect;
 module.exports = function() {
   this.World = require('../support/world.js').World;
 
-  this.When(/^I search Google for "([^"]*)"$/, function (searchQuery, next) {
+  this.When(/^I search Google for "([^"]*)"$/, function (searchQuery) {
     this.driver.get('http://www.google.co.uk/webhp?complete=0');
     this.driver.findElement({ name: 'q' })
       .sendKeys(searchQuery);
-    this.driver.findElement({ name: 'q' })
-      .sendKeys(this.webdriver.Key.ENTER)
-        .then(function() {
-          next();
-        });
+    return this.driver.findElement({ name: 'q' })
+      .sendKeys(this.webdriver.Key.ENTER);
   });
 
-  this.Then(/^I should see some results$/, function (next) {
+  this.Then(/^I should see some results$/, function () {
     this.waitFor('div.g');
-    this.driver.findElements({ css: 'div.g' })
+    return this.driver.findElements({ css: 'div.g' })
       .then(function(elements) {
         expect(elements.length).to.not.equal(0);
-        next();
       });
   });
 
